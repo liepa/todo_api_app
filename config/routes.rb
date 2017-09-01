73be030e017c0564350abce8ae59cb4f4a11a10d
Jānis Liepa <janis.liepa@tieto.com>
2017-09-01 21:59:59 +0300

    Featuer: added tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope '/api/v1/' do
     resources :tasks, except: :show
+    resources :tags, except: :show
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830201400) do
+ActiveRecord::Schema.define(version: 20170901184256) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
